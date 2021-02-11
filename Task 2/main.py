529910b3ae9baa7bbaa6534456733821ad794380
VadimPti4ka <ivakinvs@gmail.com>
2021-02-11 07:08:38 +0300

    Черновой вариант готов: скрипт умеет авторизироваться или использовать аккаунт хрома с печеньками. Далее проходит по стрницам и добирается до страницы доставки заказа, находит поле "Телефон"
    Далее - приведение к виду Page Obj, функциональность проверки на многих элементах для заказа (мб оно и не надо)

--- Task 2/main.py
+++ Task 2/main.py
@@ -6,26 +6,35 @@ from selenium import webdriver
 class PageParser:
 
     def __init__(self):
+        # Следующие настройки позволяют сохранять данные о пользователе
+        # Это дает возможность авторизироваться один раз, в последующие попытки, при использовании тех же настроек,
+        # пользователь уже будет находиться на сайте
         self.options = webdriver.ChromeOptions()
         self.options.add_argument(r"--user-data-dir=./Chrome_profile")
         self.driver = webdriver.Chrome(options=self.options)
+
         self.start_url = r"https://avito.ru"
         self.test_page_url = r"https://www.avito.ru/sochi/lichnye_veschi?cd=1&d=1"
 
     def start_browser(self):
-        # self.driver = webdriver.Chrome()
         self.go_to_page(self.start_url)
 
-        self.log_in()
+        # Кривенькая проверка на авторизацию, лучше придумать не удалось
+        # Пытаемся найти поле авторизации
+        log_in_field = self.driver.find_elements_by_class_name("header-services-menu-link-not-authenticated-3kAga")
+
+        if len(log_in_field) != 0:
+            self.log_in(log_in_field[0])
+
         # Закрываем окно
         # self.driver.quit()
 
     def go_to_page(self, url):
         self.driver.get(url)
 
-    def log_in(self):
+    def log_in(self, login_field):
         # Ищем элемент для авторизации и переходим по нему
-        self.driver.find_element_by_class_name("header-services-menu-link-not-authenticated-3kAga").click()
+        login_field.click()
 
         # Находим поле ввода логина, кликаем, вводим логин
         log_field = self.driver.find_element_by_name("login")
@@ -42,23 +51,31 @@ class PageParser:
         login_button.click()
 
 
-# Использование учетной записи чтобы не надо было логиниться
-# # ********************************************************************************************************************
-# options = webdriver.ChromeOptions()
-# options.add_argument(r"--user-data-dir=./Task 2/Chrome_profile")
-# PATH = "/Users/yourPath/Desktop/chromedriver"
-# driver = webdriver.Chrome(PATH, options=options)
-# # ********************************************************************************************************************
-
-
 # Создаем экземляр класса парсера
 test_elem = PageParser()
 # Запускаем сессию браузера
 test_elem.start_browser()
 
-# TODO Добавить проверку на авторизацию на сайте
-# Логинимся
-test_elem.log_in()
-
 # Перешли на страницу с которой будем тестировать
 test_elem.go_to_page(test_elem.test_page_url)
+
+# TODO Сделать выборку по нескольким элементам
+# # Получаем список подходящих объявлений
+# ad_list = test_elem.driver.find_elements_by_class_name("iva-item-root-G3n7v")
+# for ad in ad_list:
+#     # Находим ссылку на страницу элемента
+#     ad.find_element_by_class_name("link-link-39EVK")
+
+# Находим элемент обьявления
+ad = test_elem.driver.find_element_by_class_name("iva-item-root-G3n7v")
+# Находим в нем элемент с ссылкой и получаем ее
+next_link = ad.find_element_by_class_name("link-link-39EVK").get_attribute("href")
+
+# Перешли на страницу заказа
+test_elem.go_to_page(next_link)
+
+# Нажимаем на кнопку "Купить с доставкой"
+test_elem.driver.find_element_by_class_name("item-buyer-button-1-zak").click()
+
+# Находим поле для ввода телефона
+test_elem.driver.find_element_by_name("phone").click()
