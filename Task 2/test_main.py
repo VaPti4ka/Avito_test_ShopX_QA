4bf3c19476726be6288feb4a7de7ef4380dbe4c5
VadimPti4ka <ivakinvs@gmail.com>
2021-02-17 15:46:41 +0300

    Проверена функция авторизации, исправленна ошибка в ней
    Все готово)

--- Task 2/test_main.py
+++ Task 2/test_main.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as es
 
 from parser_main import AvitoMainPage, AvitoSearchPage, AvitoAdPage, OrderingPage
 
 
 class Test_Parser:
     def __init__(self):
-        self.options = webdriver.ChromeOptions()
-        self.driver = self.setup_settings()
+        self.driver = webdriver.Chrome()
+        # self.driver = self.setup_settings()
         # Даю время на загрузку страниц перед продолжением
         self.driver.implicitly_wait(5)
 
@@ -16,17 +18,20 @@ class Test_Parser:
         self.ad_page = None
         self.ordering_page = None
 
-    def setup_settings(self):
-        self.options.add_argument(r"--user-data-dir=./Chrome_profile")
-        return webdriver.Chrome(options=self.options)
+    @staticmethod
+    def setup_settings():
+        options = webdriver.ChromeOptions()
+        options.add_argument(r"--user-data-dir=./Chrome_profile")
+        return webdriver.Chrome(options)
 
     def test_main_page_loading(self):
         # Создаем инстанс авито мейн для (проверки) авторизации
         self.main_page = AvitoMainPage(self.driver)
         # Получаем фактический URL страницы на которой находимся и сверяем его с ожидаемым
-        assert self.main_page.get_page_url() == "https://www.avito.ru/sochi", \
+        url = self.main_page.get_page_url()
+        assert url == "https://www.avito.ru/sochi", \
             "Ошибка загрузки главной страницы Avito -> " + self.main_page.get_page_url()
-        print("PASSED:\t Загрузка главной страницы")
+        print("PASSED:\t Загрузка главной страницы " + url)
 
     def test_authorization(self):
         # Проверяем авторизирован ли уже пользователь, если нет - авторизируемся
@@ -34,47 +39,52 @@ class Test_Parser:
             self.main_page.log_in()
 
         # Проверяем наличие пункта ЛК в шапке - он есть только у авторизированного пользователя
-        assert self.main_page.search_elem([By.XPATH, "//a[@data-marker = 'header/username-button']"]), \
+        assert WebDriverWait(self.driver, 60).\
+            until(es.visibility_of_element_located((By.XPATH, "//a[@data-marker = 'header/username-button']"))), \
             "Авторизация не выполненна"
         print("PASSED:\t Авторизация")
 
     def test_search_page_loading(self):
         # Создаем инстанс страницы поиска товаров. На ней выбираем одно и получаем его ссылку
         self.search_page = AvitoSearchPage(self.driver)
-        assert self.search_page.get_page_url() == "https://www.avito.ru/sochi/lichnye_veschi?cd=1&d=1", \
+        url = self.search_page.get_page_url()
+        assert url == "https://www.avito.ru/sochi/lichnye_veschi?cd=1&d=1", \
             "Ошибка загрузки страницы поиска товаров"
-        print("PASSED:\t Загрузка страницы поиска товаров")
+        print("PASSED:\t Загрузка страницы поиска товаров " + url)
 
     def test_ad_page(self):
         # Открываем страницу конкретного товара
         link = self.search_page.get_first_ad_link()
         self.ad_page = AvitoAdPage(self.driver, link)
-        assert r"https://www.avito.ru/sochi/" in self.ad_page.get_page_url(), \
+        url = self.ad_page.get_page_url()
+        assert r"https://www.avito.ru/sochi/" in url, \
             "Ошибка перехода на страницу товара"
-        print("PASSED:\t Загрузка страницы товара")
+        print("PASSED:\t Загрузка страницы товара " + url)
 
     def test_ordering_page(self):
         # Переходим на страницу заказа товара
         self.ad_page.go_to_ordering_page()
-        # Создаем инстанс страницы заказа
+        # # Создаем инстанс страницы заказа
         self.ordering_page = OrderingPage(self.driver)
-        webdriver.support.ui.WebDriverWait(self.driver, 20)
-        url = self.ordering_page.get_page_url()
+        # После нажатия кнопки, страница загружалась слишком быстро новые данные (URL) не успевали зафиксироваться
+        # Добавил время ожидания до появления нового заголовка страницы
+        WebDriverWait(self.driver, 10).until(es.title_is("Авито — Объявления на сайте Авито"))
+        url = self.driver.current_url
         assert "https://www.avito.ru/order/checkout/" in url, \
             "Ошибка загрузки страницы заказа товара " + url
-        print("PASSED:\t Загрузка страницы заказа")
+        print("PASSED:\t Загрузка страницы заказа " + url)
 
     def test_phone_field(self):
-        # Переходим дальше на страницу оформления заказа
-        self.ordering_page = OrderingPage(self.driver)
+
         # Получаем значения поля, в которое должен вводиться телефон
         phone_field_content = self.ordering_page.get_content_phone_field()
 
         assert phone_field_content == "", "Поле для ввода телефона не пустое, почему-то"
-        print("PASSED:\t Поле ввода мобильного телефона пустое")
+        print("PASSED:\t Поле ввода мобильного телефона пустое ")
 
 
 if __name__ == '__main__':
+
     case = Test_Parser()
 
     # Загружаем и тестируем подгрузку главной страницы Avito
